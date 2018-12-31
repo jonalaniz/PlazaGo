54b386d74103ad0ad52c09aeb4d713f0ddf2c23d
Jon Alaniz <jonalaniz@jons-macbook-pro.local>
2018-12-30 18:44:08 -0600

    Initial Commit

---
+++ NWPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NWPlayer
+//
+//  Created by Jon Alaniz on 12/30/18.
+//  Copyright © 2018 Jon Alaniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
